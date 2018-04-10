55399aa45fd3740b1d8a3bb923f7c7a0fd8972d1
Eric Chrobak <eric@ericchrobak.com>
2018-04-10 00:55:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 # Your code goes here!
 class Dog
   def initialize(name)
-    @name=name
+    @name = name
   end
   def name=(name)
-    @name=name
+    @name = name
   end
   def name
     @name
